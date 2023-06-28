fe992d28bab4031a41f50339ced7ebd2b87b3b78
Henrique Cisi <henrique.cisi@accenture.com>
2023-06-27 21:36:39 -0300

    adds field widget

---
+++ lib/components/field_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:project_mine_field/models/field.dart';
+
+class FieldWidget extends StatelessWidget {
+  final Field field;
+  final void Function(Field) onOpenField;
+  final void Function(Field) onSwitchMark;
+
+  const FieldWidget(
+      {required this.field,
+      required this.onOpenField,
+      required this.onSwitchMark,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => onOpenField(field),
+      onLongPress: () => onSwitchMark(field),
+      child: _getImage(),
+    );
+  }
+
+  Widget _getImage(){
+    int mineCount = field.mineCount;
+
+    if(field.isOpen && field.hasMine && field.hasExploded){
+      return Image.asset('assets/images/bomba_0.jpeg');
+    }
+    else if (field.isOpen && field.hasMine) {
+      return Image.asset('assets/images/bomba_1.jpeg');
+    }
+    else if (field.isOpen) {
+      return Image.asset('assets/images/aberto_$mineCount.jpeg');
+    }
+    else if (field.isMarked) {
+      return Image.asset('assets/images/bandeira.jpeg');
+    }
+    else{
+       return Image.asset('assets/images/fechado.jpeg');
+    }
+  }
+}
--- lib/screens/mine_field.dart
+++ lib/screens/mine_field.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:project_mine_field/components/field_widget.dart';
 import 'package:project_mine_field/components/result_widget.dart';
+import 'package:project_mine_field/models/field.dart';
 
 class MineFieldApp extends StatelessWidget {
   const MineFieldApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    Field field = Field(line: 0, grid: 0);
+
     return MaterialApp(
       home: Scaffold(
       appBar: ResultWidget(won:true, onReset: _reset),
-      body: const Text('Board'),)
+      body: FieldWidget(field: field, onOpenField: _open, onSwitchMark: _mark)
+      ,)
       ,
     );
   }
 
   void _reset(){
-    print('aaaa');
+    print('reset');
+  }
+
+  void _open(Field field) {
+    print('open');
+  }
+
+  void _mark(Field field) {
+    print('mark');
   }
 
 }
